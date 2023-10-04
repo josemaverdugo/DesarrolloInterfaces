a9792a628bf0d5006ed93c4c79e0d0d341b96f48
josemaverdugo <123060599+josemaverdugo@users.noreply.github.com>
2023-10-04 13:29:42 +0200

    Añadido Ejercicio Lista

---
+++ Listas/Persona.java
@@ -0,0 +1,58 @@
+
+package desarrollodeinterfaces.Listas;
+
+
+public class Persona {
+
+    static Object[] toArray() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String toString() {
+        return nombre+" "+apellido;
+    }
+    
+    
+    
+   String nombre;
+   String apellido;
+   int edad;
+
+     Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+     
+     
+   
+   
+    
+    
+    
+}
